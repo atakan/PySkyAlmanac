e1e84caf95d0970d971da26510b73933e8fda351
M Atakan Gurkan <ato.gurkan@gmail.com>
2011-07-20 18:07:33 +0200

    licence (GPL)

--- A3_alm.py
+++ A3_alm.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+#    Copyright (C) 2011  Mehmet Atakan Gürkan
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License version 3 as
+#    published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program (probably in a file named COPYING).
+#    If not, see <http://www.gnu.org/licenses/>.
+
+
 from __future__ import division, print_function
 
 from math import floor, fmod, fabs, atan2, atan, asin, sqrt
