a0bf895972441079b01235ce5d0b424177cd467c
Fonda <fondatong@gmail.com>
2014-10-16 09:06:55 -0700

    adds chinese translations

--- translations.py
+++ translations.py
@@ -68,7 +68,36 @@ en = {
 }
 #Chinese
 ch = {
-
+  'midnight':'半夜',
+  'evening':'晚上',
+  'morning':'早晨',
+  'andromeda':'仙女座星系',
+  'orion':'猎户座',
+  'sevensisters':'七姐 (昴)',
+  'waning':'亏',
+  'moonrise_times':'月出时间',
+  'waxing':'打蜡',
+  'moonset_times':'月落时间',
+  'newmoon':'新月',
+  'firstquarter':'第一季',
+  'fullmoon':'满月',
+  'lastquarter':'上个季度',
+  'rising':'上升',
+  'mercury':'水星',
+  'venus':'金星',
+  'mars':'火星',
+  'jupiter':'木星',
+  'saturn':'土星',
+  'uranus':'天王星',
+  'neptune':'海王星',
+  'antares':'安塔尔',
+  'transit':'中天',
+  'transit_abbrev':ch['transit'],
+  'arcturus':'大角',
+  'pollux':'北河三',
+  'deneb':'天津四',
+  'setting':'setting',
+  'set_abbrev':ch['setting'],
 }
 #German
 de = {
@@ -89,4 +118,4 @@ if(experiment):
     else:
         t=en
 else: # otherwise manually set
-    t=en
+    t=tr
